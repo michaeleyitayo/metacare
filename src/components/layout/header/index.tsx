179a71048f9934d449c0403b9a863d77b57b55f6
michaeleyitayo <archangelmichael516@gmail.com>
2022-03-27 11:25:59 +0100

    checking

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="text-[18px] h-[78px] border-b-[1px]  border-b-[#ECEBF5">
-      Header
+      Header2
     </div>
   );
 };
